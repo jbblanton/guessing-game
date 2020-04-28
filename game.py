e2ae6d0c6870d089eae3295413a5c95e4cd4f336
Jessica Blanton <jessicabblanton@gmail.com>
2020-04-28 20:52:16 +0000

    Success, now with sassy replies

--- game.py
+++ game.py
@@ -9,15 +9,21 @@ number = random.randint(1, 101)
 count = 0
 winner = False
 while winner is False:
-    #print("Need a number")
-    guess = int(input("Pick a number between 1 & 100: "))
-    count += 1
-    if guess == number:
-        print(f"Whoa, {user_name}! You guessed correctly in {count} tries!")
-        winner = True
-    elif guess > number:
-        print("Too high, try a lower number.")
-    else:
-        print("Too low, guess higher!")
+    try:
+        guess = int(input("Pick a number between 1 & 100: "))
+    except ValueError as err:
+        print(f"Hello? {user_name}? We're asking for a NUMBER here.")
+    else:        
+        count += 1
+        if guess > 100 or guess < 1:
+            print(f"Learn to read, {user_name}; it's gotta be BETWEEN 1 and 100!")
+        elif guess == number:
+            print(f"Whoa, {user_name}! You guessed correctly in {count} tries!")
+            winner = True
+        elif guess > number:
+            print("Too high, try a lower number.")
+        else:
+            print("Too low, guess higher!")
+
     
 
