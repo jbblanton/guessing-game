85ef9dab980462d3232160b9b6f931b945d3f282
jbblanton <jessicabblanton@gmail.com>
2020-04-28 23:03:46 +0000

    User-set range

--- game.py
+++ game.py
@@ -5,13 +5,16 @@ user_name = input("What is your name?")
 print(f"Hello, {user_name}! We're going to play a guessing game!")
 print("\n")
 
-number = random.randint(1, 101)
+
+low_num = int(input("What is our low number?"))
+up_num = int(input("What is our high number?"))
+number = random.randrange(low_num, up_num + 1)
 count = 0
 
 winner = False
 while winner is False:
     try:
-        guess = int(input("Pick a number between 1 & 100: "))
+        guess = int(input(f"Pick a number between {low_num} & {up_num}: "))
     except ValueError as err:
         print(f"Hello? {user_name}? We're asking for a NUMBER here.")
     else:        
@@ -20,12 +23,13 @@ while winner is False:
             print(f"Sorry, {user_name}, you didn't guess fast enough. Better luck next time!")
             winner = True
         
-        elif guess > 100 or guess < 1:
-            print(f"Learn to read, {user_name}; it's gotta be BETWEEN 1 and 100!")
+        elif guess > up_num or guess < low_num:
+            print(f"Learn to read, {user_name}; it's gotta be BETWEEN {low_num} and {up_num}!")
         
         elif guess == number:
             print(f"Whoa, {user_name}! You guessed correctly in {count} tries!")
             winner = True
+        
         elif guess > number:
             print("Too high, try a lower number.")
         
